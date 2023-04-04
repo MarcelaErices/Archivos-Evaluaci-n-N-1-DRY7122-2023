eeaf21790a1096f3c6572d9bab50b45d4ad14b1a
NetAcad DEVASC <changeme@example.com>
2023-04-04 15:49:27 +0000

    Evaluacion N°1 DRY7122-2023

---
+++ datosjson.py
@@ -0,0 +1,6 @@
+import json
+
+with open ('myfile.json', 'r') as json_file:
+	ourjson = json.load (json_file)
+
+print(ourjson)
---
+++ evaluacion.py
@@ -0,0 +1,4 @@
+print("Evaluación N°1 Programación y Redes Virtualizadas")
+
+print("Nombre de los integrantes")
+print("Marcela Erices")
